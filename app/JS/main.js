813c09a292931ad081a4151027a6de736d925678
cchuuu <chuyuanz@nycstudents.net>
2024-12-13 12:28:10 -0500

    a

--- app/JS/main.js
+++ app/JS/main.js
@@ -1,9 +1,7 @@
-import './style.css'
-import javascriptLogo from './javascript.svg'
-import viteLogo from '/vite.svg'
-import { setupCounter } from './counter.js'
+import "../CSS/style.css";
+import { DOMselectors } from "./DOM";
 
-document.querySelector('#app').innerHTML = `
+document.querySelector("#app").innerHTML = `
   <div>
     <a href="https://vite.dev" target="_blank">
       <img src="${viteLogo}" class="logo" alt="Vite logo" />
@@ -19,6 +17,6 @@ document.querySelector('#app').innerHTML = `
       Click on the Vite logo to learn more
     </p>
   </div>
-`
+`;
 
-setupCounter(document.querySelector('#counter'))
+setupCounter(document.querySelector("#counter"));
